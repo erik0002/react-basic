a1bfe7a2d215012b2b06c1e248d1197ea61ce8f1
Yeliazar <erik.ruzhakov@mail.ru>
2022-08-07 16:40:26 +0300

    add filter year, add lifting state up

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,24 @@
+import React, {useState} from "react";
 import './Expenses.css';
 import Card from './Card';
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 
 const Expenses = (props) => {
+
+    const [changedYear, setChangedYear] = useState('2022');
+
+    const filterYearHandler = selectedYear => {
+        setChangedYear(selectedYear);
+    };
+
     return (
-        <Card className="expenses">
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={changedYear} onSelectYear = {filterYearHandler} />
+                <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
+            </Card>
+        </div>
     )
 }
 
